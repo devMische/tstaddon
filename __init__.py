191c597ef834dc76fb670b2288bd140aac9a9397
devMische <82199970+devMische@users.noreply.github.com>
2025-03-28 15:21:56 +0100

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,104 @@
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTIBILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+import bpy
+from . import addon_updater_ops
+
+from .tst_ui import TST_PT_main_panel
+
+bl_info = {
+    "name": "tstaddon",
+    "author": "DerMische",
+    "description": "this is just for testing the update functionality",
+    "blender": (4, 3, 0),
+    "version": (1, 0, 0),
+    "location": "",
+    "warning": "",
+    "category": "Generic",
+}
+
+
+@addon_updater_ops.make_annotations
+class DemoPreferences(bpy.types.AddonPreferences):
+	"""Demo bare-bones preferences"""
+	bl_idname = __package__
+
+	# Addon updater preferences.
+
+	auto_check_update = bpy.props.BoolProperty(
+		name="Auto-check for Update",
+		description="If enabled, auto-check for updates using an interval",
+		default=False)
+
+	updater_interval_months = bpy.props.IntProperty(
+		name='Months',
+		description="Number of months between checking for updates",
+		default=0,
+		min=0)
+
+	updater_interval_days = bpy.props.IntProperty(
+		name='Days',
+		description="Number of days between checking for updates",
+		default=7,
+		min=0,
+		max=31)
+
+	updater_interval_hours = bpy.props.IntProperty(
+		name='Hours',
+		description="Number of hours between checking for updates",
+		default=0,
+		min=0,
+		max=23)
+
+	updater_interval_minutes = bpy.props.IntProperty(
+		name='Minutes',
+		description="Number of minutes between checking for updates",
+		default=0,
+		min=0,
+		max=59)
+
+	def draw(self, context):
+		layout = self.layout
+
+		# Works best if a column, or even just self.layout.
+		mainrow = layout.row()
+		col = mainrow.column()
+
+		# Updater draw function, could also pass in col as third arg.
+		addon_updater_ops.update_settings_ui(self, context)
+
+		# Alternate draw function, which is more condensed and can be
+		# placed within an existing draw function. Only contains:
+		#   1) check for update/update now buttons
+		#   2) toggle for auto-check (interval will be equal to what is set above)
+		# addon_updater_ops.update_settings_ui_condensed(self, context, col)
+
+		# Adding another column to help show the above condensed ui as one column
+		# col = mainrow.column()
+		# col.scale_y = 2
+		# ops = col.operator("wm.url_open","Open webpage ")
+		# ops.url=addon_updater_ops.updater.website
+
+
+
+classes = [TST_PT_main_panel, DemoPreferences] 
+
+def register():
+    addon_updater_ops.register(bl_info)
+    for cls in classes:
+        bpy.utils.register_class(cls)
+
+
+def unregister():
+    addon_updater_ops.unregister()
+    for cls in classes:
+        bpy.utils.unregister_class(cls)
---
+++ tst_ui.py
@@ -0,0 +1,14 @@
+import bpy
+
+
+class TST_PT_main_panel(bpy.types.Panel):
+
+    bl_label = "tst-addon 1.0.0"
+    bl_idname = "TST_PT_main_panel"
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+    bl_category = 'TST'
+
+    def draw(self, context):
+        layout = self.layout
+        layout.label(text="TST ADDON 1.0.0")
